694ee291ee5bf9808c81f602198c771fe954373d
oneminimax <maxime.dion@me.com>
2017-10-17 11:55:48 -0400

    Open File plus layout
    Add an open file button. Hide the plot controls in a window.

--- HotDataViewer.py
+++ HotDataViewer.py
@@ -25,7 +25,6 @@ class HDV(wx.App):
 
         self.mainFrame.Show()
 
-
         return True
 
 
@@ -45,26 +44,24 @@ class HDVFrame(wx.Frame):
         self._makeSizer()
         self._makeBinding()
 
-        self.autoAxes = [True,True]
-        self.xplotlim = [0,1]
-        self.yplotlim = [0,1]
-
         self.dataFileList = list()
         self.dataPlotList = list()
         self.dataFollowerList = list()
 
-        dataFile = self.openDataFile('/Users/oneminimax/Documents/Projets Programmation/DataFileIO/workfile.txt')
-        self.dataFileList.append(dataFile)
-        self.currentDataFileIndex = 0
+        self.lastDataFileDir = ''
 
-        self.updateAxisChoice()
+        self.plotConfigFrame = PlotConfigFrame(self)
 
-        dataPlot = self.newPlot(dataFile)
-        dataFollower = DataFileFollower(dataFile,dataPlot,self)
-        dataFollower.start()
+        self.XAxisIndex = 0
+        self.YAxisIndex = 1
 
-        self.dataPlotList.append(dataPlot)
-        self.dataFollowerList.append(dataFollower)
+        self.XAutoLim = True
+        self.YAutoLim = True
+
+        self.xplotlim = [0,1]
+        self.yplotlim = [0,1]
+
+
 
     def _makeWidget(self):
 
@@ -75,6 +72,178 @@ class HDVFrame(wx.Frame):
         self.toolbar = NavigationToolbar(self.canvas)
         self.toolbar.Realize()
         self.toolbar.update()
+
+        self.OpenFileButton = wx.Button(self, -1, "Open File", size = (100,20))
+        self.PlotConfigButton = wx.Button(self, -1, "Plot Config", size = (100,20))
+        
+    def _makeSizer(self):
+
+        topSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        middleSizer = wx.BoxSizer(wx.HORIZONTAL)
+        middleSizer = wx.BoxSizer(wx.HORIZONTAL)
+        middleSizer.Add(self.canvas, 1, wx.EXPAND|wx.ALL, 0)
+
+        bottomSizer = wx.BoxSizer(wx.HORIZONTAL)
+        
+        bottomSizer.Add(self.OpenFileButton, 0, wx.ALL|wx.EXPAND, 0)
+        bottomSizer.Add(self.PlotConfigButton, 0, wx.EXPAND|wx.ALL, 0)
+        bottomSizer.AddStretchSpacer(1) 
+        bottomSizer.Add(self.toolbar, 0, wx.ALL|wx.EXPAND , 0)
+
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        mainSizer.Add(topSizer, 0, wx.EXPAND|wx.ALL, 5)
+        mainSizer.Add(middleSizer, 1, wx.EXPAND|wx.ALL, 5)
+        mainSizer.Add(bottomSizer, 0, wx.EXPAND|wx.ALL, 5)
+        
+        self.SetSizer(mainSizer)
+
+    def _makeBinding(self):
+
+        self.Bind(wx.EVT_CLOSE, self.onClose)
+
+        self.OpenFileButton.Bind(wx.EVT_BUTTON, self.onOpenFile)
+        self.PlotConfigButton.Bind(wx.EVT_BUTTON, self.onPlotConfig)
+
+    def openDataFile(self,dataFileName):
+
+        dataFile = DFIO.DataFileReader(dataFileName)
+
+        # self.dataFileList.append(dataFile)
+        self.dataFileList = [dataFile]
+        self.currentDataFileIndex = 0
+        self.updateAxisChoice()
+
+        dataPlot = self.newPlot(dataFile)
+        dataFollower = DataFileFollower(dataFile,dataPlot,self)
+        dataFollower.start()
+
+        # self.dataPlotList.append(dataPlot)
+        # self.dataFollowerList.append(dataFollower)
+
+        
+        self.dataPlotList = [dataPlot]
+        self.dataFollowerList = [dataFollower]
+
+    def getXY(self,dataFile):
+
+        X = dataFile.getDataByIndex(self.XAxisIndex)
+        Y = dataFile.getDataByIndex(self.YAxisIndex)
+        
+        return X,Y
+
+    def getXYDataLimit(self):
+
+        minXList = list()
+        maxXList = list()
+        minYList = list()
+        maxYList = list()
+
+        for dataFile in self.dataFileList:
+            X = dataFile.getDataByIndex(self.XAxisIndex)
+            Y = dataFile.getDataByIndex(self.YAxisIndex)
+            minXList.append(X.min())
+            maxXList.append(X.max())
+            minYList.append(Y.min())
+            maxYList.append(Y.max())
+
+        xplotlim = [np.array(minXList).min(),np.array(maxXList).max()]
+        yplotlim = [np.array(minYList).min(),np.array(maxYList).max()]
+
+        return xplotlim, yplotlim
+
+    def newPlot(self,dataFile):
+
+        X,Y = self.getXY(dataFile)
+
+        dataPlot = self.axes.plot(X, Y, color = self.colorList[0], linestyle = "solid", marker = "o")[0]
+
+        self.updatePlotLim()
+        self.canvas.draw()
+
+        return dataPlot
+
+    def updatePlot(self,plot,dataFile):
+
+        X,Y = self.getXY(dataFile)
+
+        plot.set_xdata(X)
+        plot.set_ydata(Y)
+
+        self.updatePlotLim()
+        self.canvas.draw()
+
+    def updatePlotLim(self):
+
+        xplotlim, yplotlim = self.getXYDataLimit()
+
+        if self.XAutoLim:
+            self.xplotlim = xplotlim
+            xmargin = 0.02 * (self.xplotlim[1] - self.xplotlim[0])
+            self.axes.set_xlim((self.xplotlim[0] - xmargin,self.xplotlim[1] + xmargin))
+        else:
+            pass
+
+        if self.YAutoLim:
+            self.yplotlim = yplotlim
+            ymargin = 0.02 * (self.yplotlim[1] - self.yplotlim[0])
+            self.axes.set_ylim((self.yplotlim[0] - ymargin,self.yplotlim[1] + ymargin))
+        else:
+            pass
+
+    def updateAxisChoice(self):
+
+        dataFile =  self.dataFileList[self.currentDataFileIndex]
+        fieldNameList = dataFile.dataFieldNameList
+
+        if len(fieldNameList) >= 2:
+
+            self.plotConfigFrame.XAxisChoice.SetItems(fieldNameList)
+            self.plotConfigFrame.YAxisChoice.SetItems(fieldNameList)
+
+            self.plotConfigFrame.XAxisChoice.SetSelection(self.XAxisIndex)
+            self.plotConfigFrame.YAxisChoice.SetSelection(self.YAxisIndex)
+
+    def updateAxesLabel(self):
+
+        pass
+
+    def onClose(self,event):
+        self.Destroy()
+        for dataFollower in self.dataFollowerList:
+            dataFollower.stop()
+
+        print("Hot Data Viewer Closed!")
+
+    def onOpenFile(self,event):
+
+        with wx.FileDialog(self, "Open data file", defaultDir=self.lastDataFileDir, wildcard="Data files (*.txt)|*.txt",style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return
+
+            pathName = fileDialog.GetPath()
+            try:
+                self.openDataFile(pathName)
+                self.lastDataFileDir, dump = os.path.split(pathName)
+            except IOError:
+                wx.LogError("Cannot open file.")
+
+    def onPlotConfig(self,event):
+
+        self.plotConfigFrame.Show()
+
+class PlotConfigFrame(wx.Frame):
+    def __init__(self,parent):
+        wx.Frame.__init__(self, parent, wx.ID_ANY,"Hot Data Viewer",size=(200,200))
+
+        self.HDV = parent
+
+        self._makeWidget()
+        self._makeSizer()
+        self._makeBinding()
+
+    def _makeWidget(self):
 
         self.controlPanel = wx.StaticBox(self, -1, "Axes control")
         
@@ -114,23 +283,8 @@ class HDVFrame(wx.Frame):
         controlSizer.Add(axisLabelSizer,0)
         controlSizer.Add(axisChoiceSizer,0)
         controlSizer.Add(axisAutoLimSizer,0)
-
-        topSizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        middleSizer = wx.BoxSizer(wx.HORIZONTAL)
-        middleSizer = wx.BoxSizer(wx.HORIZONTAL)
-        middleSizer.Add(self.canvas, 1, wx.EXPAND|wx.ALL, 0)
-
-        bottomSizer = wx.BoxSizer(wx.HORIZONTAL)
-        bottomSizer.Add(controlSizer, 0, wx.EXPAND|wx.ALL, 0)
-        bottomSizer.Add(self.toolbar, 0, wx.RIGHT | wx.EXPAND)
-
-        mainSizer = wx.BoxSizer(wx.VERTICAL)
-        mainSizer.Add(topSizer, 0, wx.EXPAND|wx.ALL, 5)
-        mainSizer.Add(middleSizer, 1, wx.EXPAND|wx.ALL, 5)
-        mainSizer.Add(bottomSizer, 0, wx.EXPAND|wx.ALL, 5)
         
-        self.SetSizer(mainSizer)
+        self.SetSizer(controlSizer)
 
     def _makeBinding(self):
 
@@ -142,117 +296,29 @@ class HDVFrame(wx.Frame):
         self.XAutoLimCheckBox.Bind(wx.EVT_CHECKBOX, self.onAutoLimCheckBox)
         self.YAutoLimCheckBox.Bind(wx.EVT_CHECKBOX, self.onAutoLimCheckBox)
 
-    def openDataFile(self,dataFileName):
-
-        dataFile = DFIO.DataFileReader(dataFileName)
-
-        return dataFile
-
-    def getXY(self,dataFile):
-
-        indexX = self.XAxisChoice.GetSelection()
-        indexY = self.YAxisChoice.GetSelection()
-        X = dataFile.getDataByIndex(indexX)
-        Y = dataFile.getDataByIndex(indexY)
-        
-        return X,Y
-
-    def getXYDataLimit(self):
-
-        indexX = self.XAxisChoice.GetSelection()
-        indexY = self.YAxisChoice.GetSelection()
-
-        minXList = list()
-        maxXList = list()
-        minYList = list()
-        maxYList = list()
-
-        for dataFile in self.dataFileList:
-            X = dataFile.getDataByIndex(indexX)
-            Y = dataFile.getDataByIndex(indexY)
-            minXList.append(X.min())
-            maxXList.append(X.max())
-            minYList.append(Y.min())
-            maxYList.append(Y.max())
-
-        xplotlim = [np.array(minXList).min(),np.array(maxXList).max()]
-        yplotlim = [np.array(minYList).min(),np.array(maxYList).max()]
-
-        return xplotlim, yplotlim
-
-    def newPlot(self,dataFile):
-
-        X,Y = self.getXY(dataFile)
-
-        dataPlot = self.axes.plot(X, Y, color = self.colorList[0], linestyle = "solid", marker = "o")[0]
-
-        self.updatePlotLim()
-        self.canvas.draw()
-
-        return dataPlot
-
-    def updatePlot(self,plot,dataFile):
-
-        X,Y = self.getXY(dataFile)
-
-        plot.set_xdata(X)
-        plot.set_ydata(Y)
-
-        self.updatePlotLim()
-        self.canvas.draw()
-
-    def updatePlotLim(self):
-
-        xplotlim, yplotlim = self.getXYDataLimit()
-
-        if self.XAutoLimCheckBox.IsChecked():
-            self.xplotlim = xplotlim
-            xmargin = 0.02 * (self.xplotlim[1] - self.xplotlim[0])
-            self.axes.set_xlim((self.xplotlim[0] - xmargin,self.xplotlim[1] + xmargin))
-        else:
-            pass
-
-        if self.YAutoLimCheckBox.IsChecked():
-            self.yplotlim = yplotlim
-            ymargin = 0.02 * (self.yplotlim[1] - self.yplotlim[0])
-            self.axes.set_ylim((self.yplotlim[0] - ymargin,self.yplotlim[1] + ymargin))
-        else:
-            pass
-
-    def updateAxisChoice(self):
-
-        dataFile =  self.dataFileList[self.currentDataFileIndex]
-        fieldNameList = dataFile.dataFieldNameList
-
-        if len(fieldNameList) >= 2:
-
-            self.XAxisChoice.SetItems(fieldNameList)
-            self.YAxisChoice.SetItems(fieldNameList)
-
-            self.XAxisChoice.SetSelection(0)
-            self.YAxisChoice.SetSelection(1)
-
-    def updateAxesLabel(self):
-
-        pass
-
     def onClose(self,event):
-        self.Destroy()
-        for dataFollower in self.dataFollowerList:
-            dataFollower.stop()
 
-        print("Hot Data Viewer Closed!")
+        self.Hide()
 
     def onAutoLimCheckBox(self,event):
 
-        pass
+        if event.GetEventObject() == self.XAutoLimCheckBox:
+            self.HDV.XAutoLim = self.XAutoLimCheckBox.IsChecked()
+
+        if event.GetEventObject() == self.YAutoLimCheckBox:
+            self.HDV.YAutoLim = self.YAutoLimCheckBox.IsChecked()
+
 
     def onSelectAxis(self,event):
-        
-        self.updatePlot(self.dataPlotList[self.currentDataFileIndex],self.dataFileList[self.currentDataFileIndex])
-        self.updatePlotLim()
 
+        if event.GetEventObject() == self.XAxisChoice:
+            self.HDV.XAxisIndex = self.XAxisChoice.GetSelection()
 
+        if event.GetEventObject() == self.YAxisChoice:
+            self.HDV.YAxisIndex = self.YAxisChoice.GetSelection()
+
+        self.HDV.updatePlot(self.HDV.dataPlotList[self.HDV.currentDataFileIndex],self.HDV.dataFileList[self.HDV.currentDataFileIndex])
+        self.HDV.updatePlotLim()
 
 
 class DataFileFollower(threading.Thread):
